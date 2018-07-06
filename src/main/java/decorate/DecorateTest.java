e8777d74602a6360c9d93a901f54daa268a675e5
liukai <liukai@reapal.com>
2018-07-06 11:02:57 +0800

    添加装饰模式

--- src/main/java/decorate/DecorateTest.java
+++ src/main/java/decorate/DecorateTest.java
@@ -12,5 +12,13 @@ public class DecorateTest {
 
     public static void main(String[] args) {
 
+        //创建一个需要增强的对象
+        Component component = new ConcreteComponent();
+        //创建一个装饰类
+        ConcreteDecorateA concreteDecorateA = new ConcreteDecorateA();
+        concreteDecorateA.setComponent(component);
+
+        //装饰
+        concreteDecorateA.Operation();
     }
 }
