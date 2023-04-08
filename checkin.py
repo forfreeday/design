1f508dfb50dc5e56c05843b9561cef13fbeabd7b
liukai <forfreeday@163.com>
2023-04-08 23:36:48 +0800

    add checkin

--- checkin.py
+++ checkin.py
@@ -1,54 +1,38 @@
-# -*- coding: utf8 -*-
-import requests ,os
-import json
+import requests,json,os
 
-"""
-cron: 0 9 * * *
-new Env('GLaDOS自动签到');
-"""
-
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
 sever = os.environ["SERVE"]
+# 填写server酱sckey,不开启server酱则不用填
 sckey = os.environ["SCKEY"]
+#'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
+# 填入glados账号对应cookie
 cookie = os.environ["COOKIE"]
+#'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
+
+
 
 def start():
-    checkin_url = "https://glados.one/api/user/checkin"
-    status_url = "https://glados.one/api/user/status"
-    origin = "https://glados.one"
-    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-    payload = {
-      'token': "glados.network"
-    }
+    
+    url= "https://glados.rocks/api/user/checkin"
+    url= "https://glados.rocks/console/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    referer = 'https://glados.rocks/console/checkin'
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
+   # print(res)
 
-    checkin = requests.post(checkin_url, headers = {
-      'cookie': cookie ,
-      'origin':origin,
-      'user-agent':useragent,
-      'content-type':'application/json;charset=UTF-8'
-    }, data = json.dumps(payload))
-
-    state =  requests.get(status_url, headers = {
-      'cookie': cookie ,
-      'user-agent':useragent
-    })
-
-    if sever == 'off':
-      print('消息推送开关未打开，不推送签到结果')
-    else:
-      # pushplus
-      if 'message' in checkin.text:
+    if 'message' in checkin.text:
         mess = checkin.json()['message']
-        time = state.json()['data']['leftDays'].split('.')[0]
-        requests.get('http://www.pushplus.plus/send?token=' + pushplus_token +'&title=GLaDOS自动签到&content=' + '您的Glasod账号 '+time+' 天后到期，' + mess)
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        #print(time)
+        if sever == 'on':
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+    else:
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
 
-        print("签到任务执行成功，" + '您的Glasod账号 '+ time + ' 天后到期，' + mess )
-
-        print('pushplus送消息成功')
-
-      else:
-        requests.get('http://www.pushplus.plus/send?token=' + pushplus_token +'&title=GLaDOS自动签到&content=cookie过期')
 def main_handler(event, context):
   return start()
 
 if __name__ == '__main__':
-  start()
+    start()
