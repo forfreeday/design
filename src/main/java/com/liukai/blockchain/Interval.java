b40eb6c910e64af30e77e34de006445a23b1c121
forfreeday <forfreeday@163.com>
2023-07-20 15:24:17 +0800

    abrove

---
+++ src/main/java/com/liukai/blockchain/Interval.java
@@ -0,0 +1,8 @@
+package com.liukai.blockchain;
+
+/**
+ * @author liukai
+ * @since 2022/3/14.
+ */
+public class Interval {
+}
---
+++ src/main/java/com/liukai/state/PrintUnicode.java
@@ -0,0 +1,18 @@
+package com.liukai.state;
+
+public class PrintUnicode {
+
+    public static void main(String[] args) {
+        String codepoint = "U+1F92A";   // crazy face
+        System.out.println(convertCodePoints(codepoint));
+    }
+
+    // Java, UTF-16
+    // Convert code point to unicode
+    static char[] convertCodePoints(String codePoint) {
+        Integer i = Integer.valueOf(codePoint.substring(2), 16);
+        char[] chars = Character.toChars(i);
+        return chars;
+
+    }
+}
